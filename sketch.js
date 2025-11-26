11507fc1e8807dc5b9d91468745d4db557265572
dantheman872 <95766593+dantheman872@users.noreply.github.com>
2025-11-26 11:21:41 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,14 @@ let snowflake = {
     s: []
 }
 
+let player = {
 
+    x: 100,
+    y: 100,
+    s: 50,
+    dirX: 0,
+    dirY: 0
+}
 
 function preload(){
 
@@ -22,6 +29,8 @@ function draw(){
 
     background(200,100)
     snowFall()
+    playerDraw()
+    playerMovement()
 }
 
 function snowFall(){
@@ -44,3 +53,48 @@ function snowFall(){
         snowflake.s.push(random(0.2,0.4))
     }
 }
+
+function playerMovement(){
+
+    player.x += 0.5 * player.dirX
+    player.y += 0.5 * player.dirY
+}
+
+function playerDraw(){
+
+    fill(200,50,50)
+    square(player.x, player.y, player.s)
+    square(player.x + 10, player.y - 30, 10)
+    fill(255)
+    square(player.x + player.s/4, player.y - 20, player.s /2)
+}
+
+function keyPressed(){
+
+    if(keyCode == LEFT_ARROW){
+
+        player.dirX = -1
+    } else
+
+    if(keyCode == RIGHT_ARROW){
+
+        player.dirX = 1
+    } else
+
+    if(keyCode == UP_ARROW){
+
+        player.dirY = -1
+    } else
+
+    if(keyCode == DOWN_ARROW){
+
+        player.dirY = 1
+    } else {
+
+        player.dirX = 0
+        player.dirY = 0
+    }
+
+
+}
+
